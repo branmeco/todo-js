7ac15e0dfa3c5e4af43e29be2c9a816a8f0f5a4c
branmeco <branmeco17@gmail.com>
2022-07-14 13:56:14 -0500

    Todo Class

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { Todo } from './class/todo.class.js';
+import { saludar } from './js/componentes.js';
+import './styles.css';
+
+
+
+const tarea = new Todo('Aprender JavaScript');
+
+console.log(tarea);
