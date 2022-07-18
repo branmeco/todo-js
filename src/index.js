580b26dfce154d266595b94d349e7701658645f2
branmeco <branmeco17@gmail.com>
2022-07-18 15:13:47 -0500

    Guardando y recuperando Todos

--- src/index.js
+++ src/index.js
@@ -7,16 +7,4 @@ import "./styles.css";
 
 export const todoList = new TodoList();
 
-// const tarea = new Todo("Aprender JavaScript!!!");
-// todoList.nuevoTodo( tarea );
-
-// console.log( todoList );
-
-// crearTodoHtml(tarea);
-
-// localStorage.setItem('mi-key', 'AB123');
-// sessionStorage.setItem('mi-key', 'ABC1234');
-
-// setTimeout(() => {
-//     localStorage.removeItem('mi-key')
-// }, 1500);
+todoList.todos.forEach(todo => crearTodoHtml(todo));
